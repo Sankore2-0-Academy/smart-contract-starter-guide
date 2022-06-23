8a7da392b7c0feb43bac9c8914263658dcea3038
McOtech <otienopaul36@gmail.com>
2022-06-23 10:59:04 +0300

    Persistent Collections and Storage Staking

--- contract/scripts/call.sh
+++ contract/scripts/call.sh
@@ -10,4 +10,16 @@
 
 # near call test.wajakoya.testnet attached_near --accountId wajakoya.testnet --amount 2
 
-near call test.wajakoya.testnet gas --accountId wajakoya.testnet --gas 7000000000000
+# near call test.wajakoya.testnet gas --accountId wajakoya.testnet --gas 7000000000000
+
+# near call test.wajakoya.testnet save_name '{"name": "Daramola"}' --accountId yto.testnet
+
+near view test.wajakoya.testnet get_names '{}'
+
+# near call test.wajakoya.testnet remove_name '{}' --accountId yto.testnet
+
+# near call test.wajakoya.testnet read_value '{}' --accountId yto.testnet
+
+# near call test.wajakoya.testnet increment '{}' --accountId wajakoya.testnet --amount 1
+
+# near call test.wajakoya.testnet decrement '{}' --accountId yto.testnet
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,47 +1,128 @@
 // use near_sdk::{near_bindgen, borsh::{BorshDeserialize, BorshSerialize}, setup_alloc};
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, setup_alloc, env};
+use near_sdk::{near_bindgen, setup_alloc, env, Promise};
+use near_sdk::collections::{Vector, LookupMap};
 
 setup_alloc!();
 
 #[near_bindgen]
-#[derive(Debug, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Counter {
-    value: u32
+    count: LookupMap<String, u64>,
+    names: Vector<String>
 }
 
 impl Default for Counter {
     fn default() -> Self {
-        Self { value: 0 }
+        Self { 
+            count: LookupMap::new(b"c"),
+            names: Vector::new(b"n")
+         }
     }
 }
 
 #[near_bindgen]
 impl Counter {
+    pub fn save_name(&mut self, name: String) -> String {
+        self.names.push(&name);
+        name
+    }
+
+    pub fn get_names(&self) -> Vec<String> {
+        let mut results: Vec<String> = vec![];
+        for name in self.names.iter() {
+            results.push(name.to_string());
+        }
+        results
+    }
+
+    pub fn remove_name(&mut self) {
+        self.names.pop();
+    }
+    
+
     /**
      * View function: Reading the state of blockchain
      */
-    pub fn read_value(&self) -> u32 {
-        self.value
-    }
-
-    /**
-     * Call function: Changes the state
-     */
-    pub fn decrement(&mut self) -> u32 {
-        if self.value > 0 {
-            self.value -= 1;
+    pub fn read_value(&self) -> u64 {
+        let signer = env::signer_account_id();
+        match self.count.get(&signer) {
+            Some(counter) => counter,
+            None => 0
         }
-        self.value
     }
 
     /**
      * Call function: Changes the state
      */
-    pub fn increment(&mut self) -> u32 {
-        self.value += 1;
-        self.value
+    pub fn decrement(&mut self) -> u64 {
+        let signer = env::signer_account_id();
+        if let Some(counter) = self.count.get(&signer) {
+            let mut new_counter: u64 = 0;
+            if counter > 0 {
+                new_counter = counter - 1;
+                self.count.insert(&signer, &new_counter);
+            }
+            new_counter
+        } else {
+            0
+        }
+    }
+
+    /**
+     * Call function: Changes the state
+     */
+    #[payable]
+    pub fn increment(&mut self) -> u64 {
+        let signer = env::signer_account_id();
+        let initial_storage = env::storage_usage();
+        let attached_deposit = env::attached_deposit();
+
+        if let Some(counter) = self.count.get(&signer) {
+            let new_counter = counter + 1;
+            self.count.insert(&signer, &new_counter);
+
+            let current_storage = env::storage_usage();
+            let used_storage = current_storage - initial_storage;
+            let storage_unit_price = env::storage_byte_cost();
+
+            if let Some(payable_storage_cost) = u128::checked_mul(storage_unit_price, used_storage.into()) {
+                assert!(attached_deposit >= payable_storage_cost);
+
+                let surplus = attached_deposit - payable_storage_cost;
+                let excess = if surplus > 0 { surplus } else { 0 };
+
+                if excess > 0 {
+                    let promise = Promise::new(signer);
+                    promise.transfer(excess);
+                }
+            }
+
+            new_counter
+        } else {
+            let new_counter = 1;
+            self.count.insert(&signer, &new_counter);
+
+            let current_storage = env::storage_usage();
+            let used_storage = current_storage - initial_storage;
+            let storage_unit_price = env::storage_byte_cost();
+
+            if let Some(payable_storage_cost) = u128::checked_mul(storage_unit_price, used_storage.into()) {
+                assert!(attached_deposit >= payable_storage_cost);
+
+                let surplus = attached_deposit - payable_storage_cost;
+                let excess = if surplus > 0 { surplus } else { 0 };
+
+                if excess > 0 {
+                    let promise = Promise::new(signer);
+                    promise.transfer(excess);
+                }
+                
+            }
+
+            new_counter
+        }
     }
 
     /**
